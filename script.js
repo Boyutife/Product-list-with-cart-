e9ab60723c119edffde35646ba10889125015ef6
iamBoluwatife <boluwatifeaiyenibe@gmail.com>
2025-06-09 18:02:12 +0100

    updated UI for larger screen

--- script.js
+++ script.js
@@ -31,20 +31,29 @@ function renderProducts(data) {
     const el = document.createElement('div');
     el.className = `card flex flex-col gap-1.5`;
     el.innerHTML = `
-      <h2 class="text-slate-900 text-xl font-semibold">${product.name}</h2>
-      <h3 class="-order-1 text-[hsl(14,86%,42%)]">${product.category}</h3>
-      <strong class="text-rose-500 font-semibold text-2xl">$${product.price.toFixed(
-        2
+      <h2 class="text-slate-900 text-xl font-semibold md:text-sm">${product.name}</h2>
+      <h3 class="-order-1 text-[hsl(14,86%,42%)] md:text-sm">${product.category}</h3>
+      <strong class="text-rose-500 font-semibold text-2xl md:text-sm">$${product.price.toFixed(
+        2,
       )}</strong>
       <div class="card-img -order-2 relative mb-8 rounded-2xl ">
-        <img class="card-img rounded-2xl" src="${product.image.mobile}" alt="${
-      product.name
-    }" />
-        <div class="cartBtn absolute -bottom-6 cursor-pointer left-1/2 transform -translate-x-1/2 bg-white border border-rose-600 w-1/2 px-4 py-2 flex justify-center items-center gap-2 rounded-3xl" data-id="${
+        <picture>
+  <!-- Desktop image (md and up) -->
+  <source media="(min-width: 768px)" srcset="${product.image.desktop}" />
+  
+  <!-- Fallback image for smaller screens -->
+  <img
+    class="card-img rounded-2xl"
+    src="${product.image.mobile}"
+    alt="${product.name}"
+  />
+</picture>
+
+        <div class="cartBtn absolute -bottom-6 md:-bottom-4 cursor-pointer left-1/2 transform -translate-x-1/2 bg-white border border-rose-600 w-1/2 px-3 md:px-1 py-2 flex justify-center items-center gap-2 rounded-3xl" data-id="${
           product.id
         }">
-          <img class="w-8" src="./assets/images/icon-add-to-cart.svg" alt="add cart icon" />
-          <p>Add to Cart</p>
+          <img class="w-8  md:w-4" src="./assets/images/icon-add-to-cart.svg" alt="add cart icon" />
+          <p class="text-base md:text-xs">Add to Cart</p>
         </div>
       </div>
     `;
@@ -85,14 +94,14 @@ function updateCartBtnUi(btn, product) {
   btn.innerHTML = `
       <button
         data-action="decrease"
-        class="decrease-btn border border-white rounded-full h-6 w-6 flex items-center justify-center p-0.5"
+        class="decrease-btn border border-white rounded-full h-6 w-6 md:w-4 md:h-4 flex items-center justify-center p-0.5"
       >
         <img src="./assets/images/icon-decrement-quantity.svg" alt="decrease" />
       </button>
       <p class="quantity text-white">${product.quantity}</p>
       <button
         data-action="increase"
-        class="increase-btn border border-white rounded-full h-6 w-6 flex items-center justify-center p-0.5"
+        class="increase-btn border border-white rounded-full h-6 w-6 md:w-4 md:h-4 flex items-center justify-center p-0.5 "
       >
         <img src="./assets/images/icon-increment-quantity.svg" alt="increase" />
       </button>
@@ -185,11 +194,11 @@ function updateCartForm() {
 
   const totalQuantity = [...cart.values()].reduce(
     (sum, p) => sum + p.quantity,
-    0
+    0,
   );
   const totalPrice = [...cart.values()].reduce(
     (sum, p) => sum + p.quantity * p.price,
-    0
+    0,
   );
 
   const cartItems = [...cart.values()]
@@ -199,13 +208,13 @@ function updateCartForm() {
         class="cart-item flex items-center justify-between"
       >
         <div class="cart-item-info flex flex-col gap-2.5">
-          <h3 class="text-xl font-semibold text-red-950">${product.name}</h3>
+          <h3 class="text-xl md:text-sm font-semibold text-red-950">${product.name}</h3>
           <div class="cart-item-price-info flex items-center gap-2">
             <span
-              class="item-quantity text-[hsl(14,86%,42%)] font-bold text-lg"
+              class="item-quantity text-[hsl(14,86%,42%)] font-bold text-lg md:text-xs"
               >${product.quantity}x</span
             >
-            <span class="item-unit-price text-rose-800 text-lg">
+            <span class="item-unit-price text-rose-800 text-lg md:text-sm">
               @ $${product.price.toFixed(2)}
               <span
                 class="item-total-price font-semibold ml-1 text-rose-900"
@@ -215,7 +224,7 @@ function updateCartForm() {
           </div>
         </div>
         <button
-          class="remove-item-btn w-7 h-7 p-1.5 border-2 border-slate-200 rounded-full flex items-center justify-center" data-id="${
+          class="remove-item-btn w-4 h-4 p-0.5 border-2 border-slate-200 rounded-full flex items-center justify-center" data-id="${
             product.id
           }"
         >
@@ -227,12 +236,12 @@ function updateCartForm() {
           />
         </button>
       </li>
-  `
+  `,
     )
     .join('');
 
   cartItemsContainer.innerHTML = `
-   <h2 class="text-left w-full text-2xl text-[hsl(14,86%,42%)] font-bold">
+   <h2 class="text-left w-full text-2xl md:text-xl text-[hsl(14,86%,42%)] font-bold">
           Your Cart (<span class="cart-quantity">${totalQuantity}</span>)
         </h2>
   
@@ -241,9 +250,9 @@ function updateCartForm() {
      <section
       class="order-total-section flex w-full justify-between items-center"
     >
-      <h3 class="font-semibold">Order Total</h3>
-      <div class="order-total-price text-2xl font-bold">$${totalPrice.toFixed(
-        2
+      <h3 class="font-semibold md:text-lg">Order Total</h3>
+      <div class="order-total-price text-2xl md:text-lg font-bold">$${totalPrice.toFixed(
+        2,
       )}</div>
     </section>
 
@@ -261,7 +270,7 @@ function updateCartForm() {
     </aside>
 
        <button
-      class="confirm-order-btn bg-[hsl(14,86%,42%)] text-white font-bold text-xl rounded-4xl w-full py-4"
+      class="confirm-order-btn bg-[hsl(14,86%,42%)] text-white font-bold text-xl rounded-4xl w-full py-4 md:text-lg md:py-2"
       type="submit"
     >
       Confirm Order
@@ -296,7 +305,7 @@ function updateCartForm() {
     if (cart.size > 0) {
       const totalPrice = [...cart.values()].reduce(
         (sum, p) => sum + p.quantity * p.price,
-        0
+        0,
       );
       const modal = document.getElementById('modal');
       modal.classList.remove('hidden');
@@ -314,52 +323,52 @@ function updateCartForm() {
               />
 
               <div class="cart-item-price-info flex flex-col items-start gap-2">
-                      <h3 class="text-base font-bold text-red-950 truncate w-[150px]">
+                      <h3 class="text-base md:text-sm font-bold text-red-950 truncate w-[150px]">
                       ${item.name}
                        </h3>
 
                 <div class="flex gap-3 w-full">
                   <span
-                    class="item-quantity text-[hsl(14,86%,42%)] font-bold text-base"
+                    class="item-quantity text-[hsl(14,86%,42%)] font-bold text-base md:text-sm"
                     >${item.quantity}x</span
                   >
-                  <span class="item-unit-price text-rose-900 text-base">
+                  <span class="item-unit-price text-rose-900 text-base md:text-sm">
                     @ $${item.price.toFixed(2)}</span
                   >
                 </div>
               </div>
 
-              <p class="text-lg font-bold">$${(
+              <p class="text-base font-bold">$${(
                 item.quantity * item.price
               ).toFixed(2)}</p>
             </li>
-      `
+      `,
         )
 
         .join('');
 
       modalContent.className =
-        'absolute flex flex-col bottom-0 left-0 right-0 bg-white rounded-t-4xl p-6  gap-4';
+        'absolute flex flex-col bg-white rounded-2xl p-6 gap-4 md:gap-1.5 md:w-1/4';
       modalContent.innerHTML = `
       <img
-          class="w-1/6"
+          class="w-1/6  md:w-1/12"
           src="./assets/images/icon-order-confirmed.svg"
           alt="icon-order-confirmed"
         />
-        <h2 class="text-3xl font-extrabold">Order <br />Confirmed</h2>
+        <h2 class="text-3xl md:text-xl md:font-bold font-extrabold">Order <br />Confirmed</h2>
         <p class="text-[hsl(14,86%,42%)]">We hope you enjoy your food !</p>
-        <div class="order-summary bg-rose-50 p-2.5 rounded">
+        <div class="order-summary bg-rose-50 p-2.5 rounded md:p-1">
           <ul class="p-3 space-y-1.5">
           ${cartItems}
           </ul>
           <div class="order-total flex justify-between py-3">
-            <h3>Order Total</h3>
-            <p>$${totalPrice.toFixed(2)}</p>
+            <h3 class="font-bold">Order Total</h3>
+            <p class="font-bold">$${totalPrice.toFixed(2)}</p>
           </div>
         </div>
 
         <button
-          class="start-new-order-btn bg-[hsl(14,86%,42%)] text-white font-bold text-xl rounded-4xl w-full py-3"
+          class="start-new-order-btn bg-[hsl(14,86%,42%)] text-white font-bold text-xl md:text-lg md:font-semibold md:py-2 rounded-4xl w-full py-3 cursor-pointer"
           type="button"
         >
           Start New Order
@@ -377,7 +386,7 @@ function updateCartForm() {
         products.forEach((product) => {
           product.quantity = 0;
           const btn = document.querySelector(
-            `.cartBtn[data-id="${product.id}"]`
+            `.cartBtn[data-id="${product.id}"]`,
           );
           if (btn) resetCartButton(btn);
         });
